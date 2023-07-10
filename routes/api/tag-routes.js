30e83a61b25d8e9732ebf91201a532c0cd7548c4
BondT1 <tombond248@gmail.com>
2023-07-10 20:13:12 +0100

    tag-routes.js in progress

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  console.log('All tags read');
+  Tag
+    .findAll({
+      include: [Product]
+    })
+    .then(TData => res.json(TData))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  console.log('Tag id read');
+  Tag
+    .findOne({
+      where: { id: req.params.id },
+      include: [Product]
+    })
+    .then(TData => {
+      if (!TData) {
+        res.status(404).json({ message: "Tag id invalid" });
+        return;
+      }
+      res.json(TData)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 router.post('/', (req, res) => {
